84aa18201fd20e66f13c2fe9180107aee1a6c188
John Calabrese <xchapter7x@gmail.com>
2014-04-16 21:47:11 -0400

    cleaning up dup test
    removing a duplicate test stub

--- src/accounts_test.go
+++ src/accounts_test.go
@@ -1,19 +1,1 @@
 package tradeking
-
-import (
-	"testing"
-)
-
-func SetupAccounts() {
-
-}
-
-func TearDownAccounts() {
-
-}
-
-func Test_buildEndPoint(t *testing.T) {
-    SetupAccounts()
-	defer TearDownAccounts()
-    t.Errorf("%s != %s; values should be the same", controlEndPoint, testEndPoint)
-}
