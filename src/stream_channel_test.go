d23aaf7f6d18a43b8f384c1f26c3093391d542c0
John Calabrese <xchapter7x@gmail.com>
2014-04-23 21:50:15 -0400

    adding stream_channel test coverage
    we now have some test coverage on the streamChannel object
    functionality

--- src/stream_channel_test.go
+++ src/stream_channel_test.go
@@ -1,1 +1,49 @@
 package tradeking
+
+import (
+    "testing"
+)
+
+func Test_IsAliveWhenAlive(t *testing.T) {
+    stream := NewStreamChannel()
+    defer stream.Close()
+    aliveValue := stream.IsAlive()
+    aliveControl := true
+
+    if aliveControl != aliveValue {
+        t.Errorf("%s != %s; values should be the same", aliveControl, aliveValue)
+    }
+}
+func Test_IsAliveReturnsFalseOnClose(t *testing.T) {
+    stream := NewStreamChannel()
+    stream.Close()
+    aliveValue := stream.IsAlive()
+    aliveControl := false
+
+    if aliveControl != aliveValue {
+        t.Errorf("%s != %s; values should be the same", aliveControl, aliveValue)
+    }
+}
+
+func Test_WriteStringToBuffer(t *testing.T) {
+    stream := NewStreamChannel()
+    defer stream.Close()
+    controlString := "testString"
+    go func() { stream.Write(controlString) }()
+    streamValue := <-stream.Channel
+
+    if controlString != streamValue {
+        t.Errorf("%s != %s; values should be the same", controlString, streamValue)
+    }
+}
+
+
+func Test_CloseClosesChannel(t *testing.T) {
+    stream := NewStreamChannel()
+    stream.Close()
+    _, channelOpen := <-stream.Channel
+
+    if channelOpen {
+        t.Errorf("the channel should have been closed")
+    }
+}
